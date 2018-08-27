3fdb925f6f061f9c6330281ae910e282d20eda27
sekito charlse <bozicslxye1@gmail.com>
2018-08-27 19:02:01 +0200

    [chore #160062946] add flask restplus extention

--- apicore/app.py
+++ apicore/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from instance.config import DevelopmentConfig
+from flask_restplus import Api
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object(DevelopmentConfig)
 # app.config.from_pyfile()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+api = Api(app, prefix='/api/v2', version='2.0',
+          title='StackoverFlowLite', description='Q and A api')
+
+from apicore.routes import *
