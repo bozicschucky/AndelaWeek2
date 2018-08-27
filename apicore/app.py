033382164a7a8a469caa686cf350b6202ef7768d
sekito charlse <bozicslxye1@gmail.com>
2018-08-27 18:58:24 +0200

    [chore #160062946] ad config file

--- apicore/app.py
+++ apicore/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+from instance.config import DevelopmentConfig
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_object(DevelopmentConfig)
+# app.config.from_pyfile()
 
 
 @app.route('/')
