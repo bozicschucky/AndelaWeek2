51a6f47b7c54a9c9aafcad3aa45df8ee9ab71de2
sekito charlse <bozicslxye1@gmail.com>
2018-08-28 12:38:41 +0200

    [feature #160088333] add question object

---
+++ apicore/models/questions.py
@@ -0,0 +1,7 @@
+class Question():
+    """Question class to instantiate the Question object"""
+
+    def __init__(self, title, body):
+        ''' Instantiate the Question attributes '''
+        self.title = title
+        self.body = body
