e5ed2f8ae8ec9dcff76c988802aa0f6752f8033b
sekito charlse <bozicslxye1@gmail.com>
2018-08-28 12:38:17 +0200

    [feature #160088333] add answer object

---
+++ apicore/models/answers.py
@@ -0,0 +1,6 @@
+class Answer():
+    """Answer class to instantiate the Answer object"""
+
+    def __init__(self, body):
+        ''' Instantiate the Answer attributes '''
+        self.body = body
