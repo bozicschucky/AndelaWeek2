054dbaa2f26bf3802ec45511be76ae3aa8047f0a
sekito charlse <bozicslxye1@gmail.com>
2018-08-28 14:57:27 +0200

    [feature #160088333] update questions attributes

--- apicore/models/questions.py
+++ apicore/models/questions.py
@@ -1,7 +1,8 @@
 class Question():
     """Question class to instantiate the Question object"""
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, author):
         ''' Instantiate the Question attributes '''
         self.title = title
         self.body = body
+        self.author = author
