90ea8455567a16e1cc34e8a293c2a181aeacc147
sekito charlse <bozicslxye1@gmail.com>
2018-08-28 07:58:53 +0200

    [chore #160058550] set up database tables in the class

--- apicore/models/db.py
+++ apicore/models/db.py
@@ -23,3 +23,44 @@ class DbConnect():
         print('db connection closed ')
         self.cursor.close()
         self.conn.close()
+
+    def create_table(self):
+        '''creates tables in the postgres database'''
+        commands = (
+            """
+            CREATE TABLE IF NOT EXISTS  users(
+              id serial,
+              username VARCHAR(25) UNIQUE NOT NULL,
+              password VARCHAR(100) NOT NULL,
+              PRIMARY KEY (id)
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS questions(
+              id serial,
+              user_id int NOT NULL,
+              title VARCHAR(100) NOT NULL,
+              body VARCHAR(1000) NOT NULL,
+              author VARCHAR(100) NOT NULL,
+              published_date timestamp DEFAULT CURRENT_TIMESTAMP,
+              PRIMARY KEY (id),
+              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            );
+
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  answers(
+              id serial,
+              question_id int NOT NULL,
+              body VARCHAR(2550),
+              accept_status boolean DEFAULT FALSE,
+              published_date timestabmp DEFAULT CURRENT_TIMESTAMP,
+              PRIMARY KEY (id),
+              FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
+            );
+            """,
+        )
+        for command in commands:
+            self.cursor.execute(command)
+            print('tables created successfully')
+        self.close_db_connection()
