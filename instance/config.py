a266b613c9641fe9a0344e27222d98edf6c03106
sekito charlse <bozicslxye1@gmail.com>
2018-08-30 17:22:10 +0200

    [release #159804403] fix import error on heroku

---
+++ instance/config.py
@@ -0,0 +1,19 @@
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
