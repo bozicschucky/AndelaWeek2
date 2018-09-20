db9c04e22bdbefba8e2dc97ce944598243f585db
sekito charlse <bozicslxye1@gmail.com>
2018-09-20 18:58:03 +0200

    [chore #159875590] remove reduntant test

--- apicore/tests/test_db.py
+++ apicore/tests/test_db.py
@@ -28,22 +28,6 @@ class DBModelTestCase(unittest.TestCase):
                                                     python imports', 'charlse')
         self.assertEqual(({'message': 'Question created'}, 201), question)
 
-    # def test_can_get_all_questions(self):
-    #     ''' Gets all questions for a given author '''
-    #     self.db_handler.register('charlse', 'password')
-    #     self.db_handler.create_question('I need some help',
-    #                                     'How do i fix python imports', 'charlse')
-    #     self.db_handler.create_question('I have git merge conflicts',
-    #                                     'How do i rebase this code', 'charlse')
-    #     self.db_handler.create_question(
-    #         'Writing tests not that fun', 'How do i do proper tdd', 'charlse')
-    #     self.assertEqual(dict, type(
-    #         self.db_handler.get_all_questions('charlse')))
-    #     questions = self.db_handler.get_all_questions('charlse')
-    #     self.assertEqual(3, len(questions['all_questions']))
-    #     self.assertIn('I need some help',
-    #                   questions['all_questions'][0]['title'])
-
     def tearDown(self):
         print('-----Tearing down ------------')
         self.db_handler.drop_table('users', 'answers', 'questions')
